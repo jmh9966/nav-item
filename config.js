494d7fa9c00c8e1438a8664320b280be91d88fa6
jmh9966 <dlam376@qq.com>
2025-10-08 21:23:51 +0800

    Update config.js 添加端口配置

--- config.js
+++ config.js
@@ -12,7 +12,9 @@ module.exports = {
   mysql: {
     host: process.env.MYSQL_HOST || '',
     user: process.env.MYSQL_USER || '',
+    port: process.env.MYSQL_PORT || 3306,
     password: process.env.MYSQL_PASSWORD || '',
     database: process.env.MYSQL_DATABASE || '',
   }
+
 }; 
